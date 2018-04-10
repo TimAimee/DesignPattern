d8441eb9908efa9c9046f35ac61a10a836585de4
Tim Aimee <wy_ljl@163.com>
2018-04-10 14:57:29 +0800

    Create TranslateArr.java

---
+++ src/main/java/com/pattern/behavior/ptemplate/1-Android/translate/TranslateArr.java
@@ -0,0 +1,56 @@
+package com.veepoo.simulatewatch.translate;
+
+/**
+ * Created by Administrator on 2018/4/10.
+ */
+
+public class TranslateArr extends ATranslate {
+    public String FILE_XML_ARR_GLOASS = "arrays_glossary.xml";
+
+
+    @Override
+    void setFileXMLName() {
+        this.fileXmlName = FILE_XML_ARR_GLOASS;
+    }
+
+
+    @Override
+    String translate(WorkBook sheet) {
+        if (isItem("")) {
+            getAndroidItemByKeyValue("", "");
+        } else {
+            String Key = getReallyKey("");
+            getAndroidArrayByKeyValue("", "");
+        }
+        return "";
+    }
+
+    @Override
+    String getLanguage() {
+        return "";
+    }
+
+    String getAndroidItemByKeyValue(String key, String value) {
+        if (isContainDiagonal(value)) {
+            value = replaceValue(value);
+        }
+        return "";
+    }
+
+    String getAndroidArrayByKeyValue(String key, String value) {
+        if (isContainDiagonal(value)) {
+            value = replaceValue(value);
+        }
+        return "";
+    }
+
+    boolean isItem(String key) {
+        return false;
+    }
+
+    String getReallyKey(String key) {
+        return "";
+    }
+
+
+}
