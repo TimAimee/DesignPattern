c62b0ac0a98f1fc51d361b582926e32e55155dc1
Tim Aimee <wy_ljl@163.com>
2018-04-10 14:55:50 +0800

    Create TranslateMain.java

---
+++ src/main/java/com/pattern/behavior/ptemplate/1-Android/TranslateMain.java
@@ -0,0 +1,9 @@
+class TranslateMain{
+  public static void main(String args[]){
+        ATranslate translate = new TranslateStr();
+        translate.doTranslate();
+
+        ATranslate translateArr = new TranslateArr();
+        translateArr.doTranslate();
+  }
+}
