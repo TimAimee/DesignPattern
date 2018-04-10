21ad5c225e18036ea58952d6d2d5c97d06aaea36
Tim Aimee <wy_ljl@163.com>
2018-04-10 14:57:13 +0800

    Create FileUtil.java

---
+++ src/main/java/com/pattern/behavior/ptemplate/1-Android/translate/FileUtil.java
@@ -0,0 +1,30 @@
+package translate;
+
+/**
+ * Created by Administrator on 2018/4/10.
+ */
+
+public class FileUtil {
+    public void isDirExit(String path) {
+    }
+
+    public void creatDir(String path) {
+    }
+
+
+    public void isFileExits(String path) {
+    }
+
+
+    public void deleteFile(String path) {
+    }
+
+
+    public void createFile(String path) {
+    }
+
+    public void saveToFile(String result, String path) {
+
+    }
+
+}
